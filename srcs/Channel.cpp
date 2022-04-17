05d004eaa296ff4d9860675978f2c721c5334b34
dkarthus <dkarthus@student.21-school.ru>
2022-04-17 18:42:11 +0300

    channels + poll_fds as vector

---
+++ srcs/Channel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Drahff Karthus on 4/17/22.
+//
+
+#include "Channel.hpp"
---
+++ srcs/Channel.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Drahff Karthus on 4/17/22.
+//
+
+#ifndef FT_IRC123_CHANNEL_HPP
+#define FT_IRC123_CHANNEL_HPP
+
+
+
+class Channel {
+
+};
+
+
+
+#endif //FT_IRC123_CHANNEL_HPP
